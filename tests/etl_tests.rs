7cf15381fdb9584d6ff8e8fd2b92cdd49a56d588
jiwonny29 <141780408+jiwonny29@users.noreply.github.com>
2023-10-29 22:59:31 -0400

    Update etl_tests.rs

--- tests/etl_tests.rs
+++ tests/etl_tests.rs
@@ -3,8 +3,8 @@ use jiwon_shin_sqlite::{extract, query, transform_load};
 #[test]
 fn test_extract() {
     let url =
-        "https://github.com/fivethirtyeight/data/blob/master/airline-safety/airline-safety.csv?raw=true";
-    let file_path = "data/airline-safety.csv";
+        "https://github.com/nogibjj/IDS706_Individual2_PJT/blob/main/data/cereal.csv?raw=true";
+    let file_path = "data/cereal.csv";
     let directory = "data";
 
     extract(url, file_path, directory);
@@ -14,16 +14,16 @@ fn test_extract() {
 
 #[test]
 fn test_transform_load() {
-    let dataset = "data/airline-safety.csv";
+    let dataset = "data/cereal.csv";
     let result = transform_load(dataset);
 
-    assert_eq!(result.unwrap(), "AirlineSafetyDB.db");
+    assert_eq!(result.unwrap(), "cerealDB.db");
 }
 
 #[test]
 fn test_query() {
     // Execute a SELECT query
-    let select_query = "SELECT * FROM AirlineSafetyDB WHERE airline = 'Alaska Airlines';";
+    let select_query = "SELECT * FROM cerealDB WHERE name = 'Cheerios';";
     let result = query(select_query);
 
     assert!(result.is_ok());
